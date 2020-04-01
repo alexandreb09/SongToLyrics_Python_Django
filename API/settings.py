0dfd2f754c96c50238b172a87e923282f6dae64b
Alexandre <alexandreb09@yahoo.fr>
2020-04-01 15:10:00 +0200

    :tada: Up req :tada:

--- API/settings.py
+++ API/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # phone - box 
-ALLOWED_HOSTS = ['192.168.43.187', '192.168.1.52', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*', '192.168.43.187', '192.168.1.52', 'localhost', '127.0.0.1']
