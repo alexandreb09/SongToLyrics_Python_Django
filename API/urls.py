150b41bdc0726909582826c2a9ab31fae8acb2d9
Alexandre <alexandreb09@yahoo.fr>
2020-04-02 18:37:59 +0200

    :bug: Remove routers.url :bug:

--- API/urls.py
+++ API/urls.py
@@ -23,8 +23,9 @@ router = routers.DefaultRouter()
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
+    path('', test),
     path('test/', test),
     path('find_sound/', mon_super_test),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
